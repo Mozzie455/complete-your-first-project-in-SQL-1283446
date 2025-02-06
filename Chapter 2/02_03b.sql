7936659c7053bd47f040ddb4831b26281d751459
mozzie <joshuamutiso455@gmail.com>
2025-02-06 23:38:49 +0300

    Exploring Sales Data

--- Chapter 2/02_03b.sql
+++ Chapter 2/02_03b.sql
@@ -1,1 +1,9 @@
 -- Determine which sales people made no sales
+SELECT 
+  SalesPerson.SalepersonID,
+  FirstName,
+  LastName
+FROM SalesPerson
+LEFT OUTER JOIN Orders
+ON SalesPerson.SalepersonID = Orders.SalepersonID
+WHERE Orders.SalepersonID IS NULL;  
