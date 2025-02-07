735508477705293cd9a1a6c1618880e408b853b6
mozzie <joshuamutiso455@gmail.com>
2025-02-07 13:20:14 +0300

    Exploring Customer Data

--- Chapter 3/03_04b.sql
+++ Chapter 3/03_04b.sql
@@ -1,1 +1,11 @@
 -- Determine what products sold together
+SELECT
+a.ProductsID as ProductsID1,
+b.ProductsID as ProductsID2
+COUNT(*) as TimePurchased
+FROM OrderItem as a 
+INNER JOIN OrderItem as b
+ON a.OrderID = b.OrderID
+AND a.ProductsID < b.ProductsID
+GROUP BY a.ProductsID, b.ProductsID
+ORDER BY TimePurchased DESC;
